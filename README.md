0e65c4e8ac800216332340af5987bf31ebc84c7f
Fatslim Joe <energy2809@gmail.com>
2023-05-08 15:21:36 +0200

    upload

---
+++ README.md
@@ -0,0 +1,42 @@
+# clean-graphite-whispers-files
+Purge stale Whisper’s files with clean-graphite-whisper-files.py
+
+We have developed a new graphite cleaner script specifically designed to purge old stale whisper data. The existing community scripts were not well-suited for our environment as they generated some warnings and were written in Python 2. Furthermore, they lacked information about the age of stale graphite files, which could be useful in our case.
+
+Before you begin using the script, please verify the path of your interpreter. Usually, the default path is: `#!/usr/bin/python3`
+
+## IMPORTANT
+
+
+Before using the script, it is recommended to estimate the total size of your stale whisper files. You can use the --dry-run option, which will not take any action but will provide a summary of what should be deleted.
+
+If you have a large number of files and the whisper files are not on a separate disk, it may significantly impact the performance of your Graphite server due to high disk I/O. Therefore, it is recommended to execute the script in parts, gradually reducing the number of '--days' parameter each time.
+
+Exercise caution when using the -l argument, which enables logging and stores logs in /var/log/graphite_cleanup_yyyy_mm_dd.log. This can result in additional disk I/O and potentially affect the performance of your Graphite server and could use a lot of additional storage place.
+
+
+## Usage
+
+Example,
+
+`./clean-graphite-whisper-files.py -s -p /opt/graphite/storage/whisper --dry-run`
+
+To find more option, you can use -h argument:
+
+```
+./clean-graphite-whispers-files.py -h
+usage: clean-graphite-whispers-files.py [-h] -d DAYS [-i] -p PATH [-l] [-s]
+
+Retrieve old obsolete whisper files and delete them!
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -d DAYS, --days DAYS  Older then, number of days, default is 365 days
+  -i, --dry-run         Show me only what it could be deleted, without delete
+                        action!
+  -p PATH, --path PATH  Path/location of whisper files, dafault:
+                        /opt/graphite/storage/whisper
+  -l, --log             Enable logging, dafault:
+                        /var/log/graphite_cleanup_yyyy_mm_dd.log
+  -s, --show            Show me output of each deleted file!
+```
---
+++ clean-graphite-whispers-files.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python3
+
+import os
+import time
+import logging
+import argparse
+
+def main():
+	parser = argparse.ArgumentParser(description='Retrieve old obsolete whisper files and delete them!')
+	parser.add_argument('-d', '--days', required=True, help='Older then, number of days, default is 365 days')
+	parser.add_argument('-i', '--dry-run', action='store_true', help='Show me only what it could be deleted, without delete action!')
+	parser.add_argument('-p', '--path', required=True, default='/opt/graphite/storage/whisper', help='Path/location of whisper files, dafault: /opt/graphite/storage/whisper')
+	parser.add_argument('-l', '--log', action='store_true', help='Enable logging, dafault: /var/log/graphite_cleanup_yyyy_mm_dd.log')
+	parser.add_argument('-s', '--show', action='store_true', help='Show me output of each deleted file!')
+	parser.set_defaults(dry_run=False)
+	parser.set_defaults(log=False)
+	parser.set_defaults(show=False)
+	
+	args = parser.parse_args()
+
+	if args.dry_run:
+		dry_run = True
+	else:
+		dry_run = False
+
+	if args.days:
+		selected_days=int(args.days)
+	else:
+		selected_days = 365
+	
+	if args.path:
+		root_directory = args.path
+	else:
+		root_directory = "/opt/graphite/storage/whisper"
+
+	if args.log:
+		log_filename = f"/var/log/graphite_cleanup_{time.strftime('%Y_%m_%d')}.log"
+		logging.basicConfig(filename=log_filename, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+	root_directory = '/opt/graphite/storage/whisper/'
+
+	# Get the current time in seconds since the epoch
+	current_time = time.time()
+
+	# Calculate the number of seconds
+	selected_days = selected_days * 24 * 60 * 60
+	counter = 0
+	total_size = 0
+	total_del_size = 0
+	file_size = 0
+	file_del_size = 0
+
+	#CHECKING AND DELETING OLD WHISPER FILES
+	# Loop through each subdirectory and file in the root directory
+	for dirpath, dirnames, filenames in os.walk(root_directory):
+		for filename in filenames:
+			if filename.endswith('.wsp'):
+				file_path = os.path.join(dirpath, filename)
+				# Get the modification time of the file in seconds since the epoch
+				file_time = os.path.getmtime(os.path.join(dirpath, filename))
+				# Format the modification time as a human-readable string
+				formatted_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(file_time))
+				# Calculate the age of the file in seconds
+				file_age = current_time - file_time
+				file_size = os.path.getsize(file_path)
+				total_size += file_size 
+				if file_age > selected_days:
+					file_del_size = os.path.getsize(file_path)
+					counter = counter + 1
+					total_del_size += file_del_size
+					
+					if not args.dry_run:
+						os.remove(os.path.join(dirpath, filename))
+
+					if args.log:
+						logging.info(f"{file_path} (age: {file_age/86400:.1f} days, size: {file_size/1048576:.2f} MB, last update: {formatted_time})")
+
+					if args.show:
+						print(f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] {file_path} (age: {file_age/86400:.1f} days, last update: {formatted_time})")
+
+					if not args.dry_run:
+						print(f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] Deleted {file_path}!")
+						logging.info(f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] Deleted {file_path}!")
+
+	if args.show:
+		print("\n-----------------------------------------------------------------------------------------------------------------------------------------")
+	print(f"Total Deleted whisper files {counter}, Total deleted size: {total_del_size/(1024*1024):.2f} MB, Total size of all whisper files: {total_size/(1024*1024*1024):.6f} GB, Ratio: {float(float(total_del_size)/float(total_size))*100:.2f}% deleted")
+
+if __name__ == '__main__':
+	main()
